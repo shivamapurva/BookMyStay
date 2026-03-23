ef5a61272542531d9babf0a6e8329539935a6026
shivamapurva <apurvashivam11@gmail.com>
2026-03-23 11:09:41 +0530

    UC1

---
+++ BookMyStay/src/BookMyStay.java
@@ -0,0 +1,20 @@
+
+
+public class BookMyStay {
+
+        public static void main(String[] args) {
+
+            // Display welcome message
+            System.out.println("======================================");
+            System.out.println(" Welcome to Book My Stay Application ");
+            System.out.println(" Hotel Booking System v1.0 ");
+            System.out.println("======================================");
+
+            // Inform user that application started successfully
+            System.out.println("Application started successfully.");
+
+            // Application ends here
+            System.out.println("Thank you for using Book My Stay!");
+        }
+    }
+
